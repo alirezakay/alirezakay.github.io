51ad23b308e414d6fa727b0123e2faefa074c8f7
alireza kavian <kay.alireza@gmail.com>
2019-12-10 15:51:19 +0330

    added demo f.sheikhi

---
+++ demo/farnaz-sheikhi/precache-manifest.7c64322a5bcefd190df40b284a4eb918.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49f7e429775e36c6ffd6def6055b162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52b1871f1e038876ba5b",
+    "url": "/static/css/main.c1e385bc.chunk.css"
+  },
+  {
+    "revision": "f03a3017c20b10a60853",
+    "url": "/static/js/2.5d691298.chunk.js"
+  },
+  {
+    "revision": "ab06775db0006a1715066c989bcfff13",
+    "url": "/static/js/2.5d691298.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52b1871f1e038876ba5b",
+    "url": "/static/js/main.b2e6e88d.chunk.js"
+  },
+  {
+    "revision": "1ecc85714e3163f9666a",
+    "url": "/static/js/runtime-main.b910407d.js"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/static/media/Roboto.df7b648c.ttf"
+  },
+  {
+    "revision": "3f9b77c22cf6f31f4a1fc399f2475901",
+    "url": "/static/media/course.3f9b77c2.jpg"
+  },
+  {
+    "revision": "ebff7904436516bad20042d9e5bd2a37",
+    "url": "/static/media/logo.ebff7904.png"
+  }
+]);

6cf9edaf37d6d5e2df8b7924bdfac255dda962a5
alireza kavian <kay.alireza@gmail.com>
2020-05-06 20:01:55 +0430

    cvlab10

--- courses/cvlab/cvlab10/script.js
+++ courses/cvlab/cvlab10/script.js
@@ -2,6 +2,27 @@ $(function(){
   const score = $("#calc").find("#score");
   const submit = $("#calc").find("#submit");
   const result = $("#calc").find("#result");
+  const exp = $("#exp");
+  
+  const inter = setInterval(() => {
+    const endTime = new Date("2020/5/13-12:00:00");
+    const startTime = new Date();
+    const dur = (endTime - startTime)/1000; // In seconds
+    if(dur <= 1){
+      console.log("DOODLE TIME SELECTION HAS BEEN ENDED!");
+      clearInterval(inter);
+      $("#doodle").attr("href", "https://doodle.com").addClass("disabled").text("Doodle Time Selection Link (EXPIRED)");
+    }
+    let days = dur/(24*60*60);
+    let hours = (days - Math.floor(days)) * 24;
+    let mins = (hours - Math.floor(hours)) * 60;
+    let secs = (mins - Math.floor(mins)) * 60;
+    days = Math.floor(days);
+    hours = Math.floor(hours);
+    mins = Math.floor(mins);
+    secs = Math.floor(secs);
+    exp.html(`<b>${days}</b> days <b>${hours}</b> hours <b>${mins}</b> mins <b>${secs}</b> secs`);
+  }, 1000);
   
   score.change(function(e){
     const val = parseInt(e.target.value, 10);
